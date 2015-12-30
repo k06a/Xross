6748a17e9d0dc98a793422ccbbdd0dd01f5ba78a
Anton Bukov <k06aaa@gmail.com>
2015-12-30 13:56:02 +0300

    KYXrossViewController updated to 1.1.3 to handle keyboard on VCs

--- KYXrossViewController.podspec
+++ KYXrossViewController.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "KYXrossViewController"
-  s.version          = "0.1.2"
+  s.version          = "0.1.3"
   s.summary          = "All-directions-enabled UIPageViewController"
 
   s.homepage         = "https://github.com/Searchie/frontend"
--- Pod/KYXrossScrollView.m
+++ Pod/KYXrossScrollView.m
@@ -8,17 +8,63 @@
 
 #import "KYXrossScrollView.h"
 
+static __weak id currentFirstResponder_private;
+
+@implementation UIResponder (FirstResponder)
+
++ (id)currentFirstResponder {
+    currentFirstResponder_private = nil;
+    [[UIApplication sharedApplication] sendAction:@selector(findFirstResponder:) to:nil from:nil forEvent:nil];
+    return currentFirstResponder_private;
+}
+
+- (void)findFirstResponder:(id)sender {
+    currentFirstResponder_private = self;
+}
+
+@end
+
+//
+
 @interface UIScrollView () <UIGestureRecognizerDelegate>
 
 @end
 
+//
 
 @interface KYXrossScrollView ()
 
 @end
 
-
 @implementation KYXrossScrollView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self addObserver:self forKeyPath:@"contentOffset" options:(NSKeyValueObservingOptionNew) context:NULL];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"contentOffset"];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    id responder = [UIResponder currentFirstResponder];
+    if ([responder isKindOfClass:[UIView class]]) {
+        UIView *view = ((UIView *)responder).inputAccessoryView;
+        
+        CGPoint p = [self convertPoint:CGPointZero fromView:responder];
+        CGFloat k = CGPointEqualToPoint(self.contentOffset, CGPointZero) ? 0.0 : 1.0;
+        view.transform = CGAffineTransformMakeTranslation(-self.contentOffset.x*k + floor(p.x/self.frame.size.width)*self.frame.size.width, 0);
+        
+        for (UIWindow *window in [UIApplication sharedApplication].windows) {
+            if ([NSStringFromClass(window.class) isEqualToString:@"UIRemoteKeyboardWindow"]) {
+                window.transform = view.transform;
+            }
+        }
+    }
+}
 
 // Avoid UITextField to scroll superview to become visible on becoming first responder
 - (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated {
